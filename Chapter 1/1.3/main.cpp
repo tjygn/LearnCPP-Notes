e1c3d591ea374844436acf09a0335e5ae85ac562
tjygn <ahreth5656@gmail.com>
2026-05-03 11:26:35 +0300

    Reorganized plus added 1.4 example

---
+++ Chapter 1/1.3/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+int main() {
+    std::cout << 5;
+    std::cout << -6.7;
+    std::cout << 'H';
+    std::cout << "Hell0";
+    return 0;
+}
---
+++ Chapter 1/1.4/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int width;
+    width = 5;
+
+    std::cout << width;
+
+    width = 7;
+
+    std::cout << width;
+    //the program works even though I deleted the return 0
+}
