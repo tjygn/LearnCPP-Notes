1ca64b8851e9260b3d63d064d2f635872a017638
tjygn <ahreth5656@gmail.com>
2026-05-03 11:37:55 +0300

    Added initialization examples in 1.4

--- Chapter 1/1.4/main.cpp
+++ Chapter 1/1.4/main.cpp
@@ -9,5 +9,13 @@ int main() {
     width = 7;
 
     std::cout << width;
+
+    int height {6}; //Looks weird but it is more correct cuz provokes an error if wrong value is passed
+
+    int volume = 8.5; //Doesnt provoke an error on float value
+
+    std::cout << volume;
+    std::cout << height;
+
     //the program works even though I deleted the return 0
 }
